d42309482c233c6df71958294779995863cb07ae
murillo dias nunes <mdngtr09@gmail.com>
2025-04-07 20:45:12 -0300

    Implementada a função venderProduto() no ProdutosDAO

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -68,4 +68,20 @@ public class ProdutosDAO {
         return listagem;
     }
 
+    public void venderProduto(int id) {
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            prep.setInt(1, id);
+
+            prep.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Produto vendido com sucesso!");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao vender produto: " + e.getMessage());
+        }
+    }
+
 }
